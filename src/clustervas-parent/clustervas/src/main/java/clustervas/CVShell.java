4108b951f1034a65c9c72f8df538b3aa1a70acde
hadidilek <hadi.dilek@gmail.com>
2020-03-09 07:35:55 +0300

    shell console

---
+++ src/clustervas-parent/clustervas/src/main/java/clustervas/CVShell.java
@@ -0,0 +1,35 @@
+package clustervas;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.shell.standard.ShellComponent;
+import org.springframework.shell.standard.ShellMethod;
+import org.springframework.shell.standard.ShellOption;
+
+import clustervas.service.CVAgent;
+import clustervas.service.CVService;
+
+@ShellComponent
+public class CVShell {
+
+	private static final int MAX_HOST_LIMIT = 5000;
+
+	@Autowired
+	private CVService cvService;
+
+	@Autowired
+	private CVAgent agent;
+
+	@ShellMethod("Generate key-pair")
+	public void genKeys() throws Exception {
+
+	}
+
+	@ShellMethod("Mevcut bir musterinin lisansini yeniden uretir")
+	public void renewLicense(String guid, int validMonthCount, @ShellOption(defaultValue = "-1") int hostLimit) {
+		
+		
+	}
+
+	// ------
+
+}
